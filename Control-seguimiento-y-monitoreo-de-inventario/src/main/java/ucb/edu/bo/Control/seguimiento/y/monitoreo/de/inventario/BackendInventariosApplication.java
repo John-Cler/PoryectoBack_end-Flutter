c90a0331804bf778b504e094e042c4fd42d67c68
John Cler <choquejohn6@gmail.com>
2024-05-17 23:04:07 -0400

    Creando proyecto Backend

---
+++ Control-seguimiento-y-monitoreo-de-inventario/src/main/java/ucb/edu/bo/Control/seguimiento/y/monitoreo/de/inventario/BackendInventariosApplication.java
@@ -0,0 +1,13 @@
+package ucb.edu.bo.Control.seguimiento.y.monitoreo.de.inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInventariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendInventariosApplication.class, args);
+	}
+
+}
---
+++ Control-seguimiento-y-monitoreo-de-inventario/src/test/java/ucb/edu/bo/Control/seguimiento/y/monitoreo/de/inventario/BackendInventariosApplicationTests.java
@@ -0,0 +1,13 @@
+package ucb.edu.bo.Control.seguimiento.y.monitoreo.de.inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendInventariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
