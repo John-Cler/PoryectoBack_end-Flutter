6a9bb6e22127974c74c295341a66d8156c00741f
John Cler <choquejohn6@gmail.com>
2024-06-03 18:51:20 -0400

    Crear entity de bloque

---
+++ src/main/java/ucb/edu/bo/Proyecto/Entity/Bloque.java
@@ -0,0 +1,34 @@
+package ucb.edu.bo.Proyecto.Entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "bloques")
+public class Bloque {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="nombre")
+    private String nombre;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
