4f013563c1442969e235be81b4ebbcbbf616a328
John Cler <choquejohn6@gmail.com>
2024-06-03 18:41:14 -0400

    Crear entity de Asignacion de equipo

---
+++ src/main/java/ucb/edu/bo/Proyecto/Entity/AsignacionEquipo.java
@@ -0,0 +1,60 @@
+package ucb.edu.bo.Proyecto.Entity;
+
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "asignacion_equipos")
+public class AsignacionEquipo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @OneToOne
+    @JoinColumn(name = "id_equipo")
+    private Equipo equipo;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_administrativo",referencedColumnName = "id")
+    private Administrativo administrativo;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_user", referencedColumnName = "id")
+    private User user;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Equipo getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(Equipo equipo) {
+        this.equipo = equipo;
+    }
+
+    public Administrativo getAdministrativo() {
+        return administrativo;
+    }
+
+    public void setAdministrativo(Administrativo administrativo) {
+        this.administrativo = administrativo;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
+
