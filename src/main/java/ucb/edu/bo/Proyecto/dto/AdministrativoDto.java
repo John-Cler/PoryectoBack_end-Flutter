d9e0c58b5d35803f7a6fe055f3c88621e7af648c
John Cler <choquejohn6@gmail.com>
2024-06-04 00:28:42 -0400

    Acrear equipoDto

--- src/main/java/ucb/edu/bo/Proyecto/dto/AdministrativoDto.java
+++ src/main/java/ucb/edu/bo/Proyecto/dto/AdministrativoDto.java
@@ -1,4 +1,65 @@
 package ucb.edu.bo.Proyecto.dto;
 
+import ucb.edu.bo.Proyecto.entity.AsignacionEquipo;
+import ucb.edu.bo.Proyecto.entity.Bloque;
+import ucb.edu.bo.Proyecto.entity.Departamento;
+import ucb.edu.bo.Proyecto.entity.User;
+
+import java.util.List;
+
 public class AdministrativoDto {
+    private Integer id;
+    private Integer id_user;
+    private DepartamentoDto departamentoDto;
+    private Integer id_departamento;
+    private BloqueDto bloqueDto;
+    private Integer id_bloque;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public DepartamentoDto getDepartamentoDto() {
+        return departamentoDto;
+    }
+
+    public void setDepartamentoDto(DepartamentoDto departamentoDto) {
+        this.departamentoDto = departamentoDto;
+    }
+
+    public BloqueDto getBloqueDto() {
+        return bloqueDto;
+    }
+
+    public void setBloqueDto(BloqueDto bloqueDto) {
+        this.bloqueDto = bloqueDto;
+    }
+
+    public Integer getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(Integer id_user) {
+        this.id_user = id_user;
+    }
+
+    public Integer getId_departamento() {
+        return id_departamento;
+    }
+
+    public void setId_departamento(Integer id_departamento) {
+        this.id_departamento = id_departamento;
+    }
+
+    public Integer getId_bloque() {
+        return id_bloque;
+    }
+
+    public void setId_bloque(Integer id_bloque) {
+        this.id_bloque = id_bloque;
+    }
 }
---
+++ src/main/java/ucb/edu/bo/Proyecto/dto/EquipoDto.java
@@ -0,0 +1,112 @@
+package ucb.edu.bo.Proyecto.dto;
+
+import ucb.edu.bo.Proyecto.entity.AsignacionEquipo;
+import ucb.edu.bo.Proyecto.entity.Bloque;
+import ucb.edu.bo.Proyecto.entity.Departamento;
+import ucb.edu.bo.Proyecto.entity.EquipoParte;
+//import jakarta.persistence.*;
+
+import java.util.List;
+
+public class EquipoDto {
+    private Integer id;
+    private String tipo;
+    private Integer id_departamento;
+    private DepartamentoDto departamentoDto;
+    private Integer id_bloque;
+    private BloqueDto bloqueDto;
+    private String descripcion;
+    private String nro_activo;
+    private String nro_serie;
+    private String marca;
+    private String modelo;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getId_departamento() {
+        return id_departamento;
+    }
+
+    public void setId_departamento(Integer id_departamento) {
+        this.id_departamento = id_departamento;
+    }
+
+    public DepartamentoDto getDepartamentoDto() {
+        return departamentoDto;
+    }
+
+    public void setDepartamentoDto(DepartamentoDto departamentoDto) {
+        this.departamentoDto = departamentoDto;
+    }
+
+    public Integer getId_bloque() {
+        return id_bloque;
+    }
+
+    public void setId_bloque(Integer id_bloque) {
+        this.id_bloque = id_bloque;
+    }
+
+    public BloqueDto getBloqueDto() {
+        return bloqueDto;
+    }
+
+    public void setBloqueDto(BloqueDto bloqueDto) {
+        this.bloqueDto = bloqueDto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNro_activo() {
+        return nro_activo;
+    }
+
+    public void setNro_activo(String nro_activo) {
+        this.nro_activo = nro_activo;
+    }
+
+    public String getNro_serie() {
+        return nro_serie;
+    }
+
+    public void setNro_serie(String nro_serie) {
+        this.nro_serie = nro_serie;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+}
