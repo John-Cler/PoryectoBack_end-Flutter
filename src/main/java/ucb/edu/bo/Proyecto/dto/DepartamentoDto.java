cd7fa7047aa204dcf1539098b1102a7e4d523ebb
John Cler <choquejohn6@gmail.com>
2024-06-04 00:15:47 -0400

    Crear Dto de departamento

---
+++ src/main/java/ucb/edu/bo/Proyecto/dto/DepartamentoDto.java
@@ -0,0 +1,27 @@
+package ucb.edu.bo.Proyecto.dto;
+
+public class DepartamentoDto {
+    private Integer id;
+    private String nombre;
+
+    public DepartamentoDto(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
